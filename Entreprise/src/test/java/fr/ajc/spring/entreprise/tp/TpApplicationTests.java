1c0c87ea0de1931c1e9e291d0f6ddc59a43e297d
Alex Metsemakers <a_metsemakers@hotmail.com>
2020-01-13 15:19:28 +0100

    initial commit

---
+++ Entreprise/src/test/java/fr/ajc/spring/entreprise/tp/TpApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.ajc.spring.entreprise.tp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
